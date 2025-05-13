17219138283426b53e535cd567adef9106fcdad7
chompypotato <fletcher.hoch@outlook.com>
2025-05-13 07:27:10 -0400

    Update uv.config.js

--- flux/uv/uv.config.js
+++ flux/uv/uv.config.js
@@ -1,1 +1,10 @@
-
+self.__uv$config = {
+    prefix: '/flux/go/',
+    bare:'https://bare2.mysticmath.workers.dev/', //this is the line you will change to your server
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/flux/uv/uv.handler.js',
+    bundle: '/flux/uv/uv.bundle.js',
+    config: '/flux/uv/uv.config.js',
+    sw: '/flux/uv/uv.sw.js',
+};

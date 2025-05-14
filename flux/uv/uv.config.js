8033ea2eb3703ce8f39664bf2a7bde043e9b1a22
chompypotato <fletcher.hoch@outlook.com>
2025-05-14 16:10:42 -0400

    Update uv.config.js

--- flux/uv/uv.config.js
+++ flux/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/flux/go/',
-    bare:'https://bare2.mysticmath.workers.dev/', //this is the line you will change to your server
+    bare:'https://schoolwork-nine.vercel.app/', //this is the line you will change to your server
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/flux/uv/uv.handler.js',
